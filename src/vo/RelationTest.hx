98e83a1fb9dd57bf028bb8090422c9fd5d8ed094
postite <blog@postite.com>
2011-10-03 09:43:53 +0200

    moved to spod_macro

---
+++ src/vo/RelationTest.hx
@@ -0,0 +1,51 @@
+package vo;
+import sys.db.Manager;
+import sys.db.Object;
+import sys.db.Types;
+import microbe.vo.Spodable;
+import microbe.form.IMicrotype;
+
+@:table("relationtest")
+class RelationTest extends Object, implements Spodable
+{
+	
+	
+	public var poz:Int;
+	public var id: SId;
+	public var titre:SString<255>;
+	@:skip public var childListe:List<ChildTest>;
+	static public var manager=new RelationManager(RelationTest);
+	
+	/// interface vo spodable
+	public function getFormule():Hash<FieldType> 
+	{
+		var formule:Hash<microbe.form.FieldType>;
+		formule = new Hash<microbe.form.FieldType>();
+		formule.set("titre", {classe:"microbe.form.elements.AjaxInput",type:formElement,champs:titre});
+			formule.set("childListe",{classe:"vo.ChildTest",type:collection,champs:childListe});
+		return formule;
+	}
+	 public function getDefaultField():String{
+		return titre.toString();
+	}
+	
+	
+}
+
+
+class RelationManager extends Manager<RelationTest>{
+	
+	//called when a returned SPOD object is created following data retrieval from the database
+		override function make(a:RelationTest) : Void {
+			trace("make");
+			a.childListe=ChildTest.manager.search({rid:a.id});
+		}
+	
+	//called just before an update is made
+	override function unmake( a : RelationTest ) : Void
+	{
+		trace("unmake");
+		a.childListe=ChildTest.manager.search({rid:a.id});
+	}
+	
+}
