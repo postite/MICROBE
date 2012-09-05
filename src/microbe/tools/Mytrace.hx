b71a9c42f271c706b9c95217d888be58e065fbda
postite <blog@postite.com>
2012-09-05 13:06:43 +0200

    added Tracing redirections

---
+++ src/microbe/tools/Mytrace.hx
@@ -0,0 +1,18 @@
+package microbe.tools;
+class Mytrace {
+
+    public static function setRedirection() {
+        haxe.Log.trace = mytrace;
+    }
+
+    private static function mytrace( v : Dynamic, ?inf : haxe.PosInfos ) {
+        #if js
+        		untyped console.log(v +" ::> \n " +inf.fileName +" "+inf.lineNumber+" "+inf.methodName);
+        #end
+
+        #if php
+        		microbe.controllers.GenericController.appDebug.log(v +" ::> \n " +inf.fileName +" "+inf.lineNumber+" "+inf.methodName);
+        #end
+    }
+
+}
