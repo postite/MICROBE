5a6eaf28cfabcc01d1bbe8913ba1cbc4decf43c3
postite <blog@postite.com>
2012-09-21 15:57:32 +0200

    moved all methods to TagMAnager

--- src/vo/Taxo.hx
+++ src/vo/Taxo.hx
@@ -24,132 +24,131 @@ class Taxo extends Object
 		    return[ {prop:"child", key:"child_id", manager:CrossVo.manager}];
 	}*/
 	
-	public static var manager = new TagManager();
+	public static var manager = new TaxoManager();
 	
 }
 
 //used to be accessed only by TagManager
-typedef FriendlyTaxoManager={
-	private function getTags( spod:String ) : List <Taxo >;
-	private function getTag( tag:String , ?spod:String ) : Taxo
-}
+
 class TaxoManager extends Manager<Taxo>
 {
-	 var currentspod:String;
+	 //var currentspod:String;
 	public function new()
 	{
 	super(Taxo);
 
 	//ChapterManager.amgr = this;
 	}
- function getTags( spod:String ) : List <Taxo >
-{
+//  function getTags( spod:String ) : List <Taxo >
+// {
 	
-	var spodTable=getSpodTable(spod);
-	trace("spodTAble="+spodTable);
-	var resultSet=Manager.cnx.request("
-		SELECT distinct TX.* from taxo AS TX
-		JOIN tagSpod AS TS ON TS.tag_id=TX.taxo_id
-		JOIN "+spodTable+" AS SP ON SP.id=TS.spod_id
-		WHERE TX.spodtype='"+spod.toLowerCase()+"'"
-		);
+// 	var spodTable=getSpodTable(spod);
+// 	trace("spodTAble="+spodTable);
+// 	var resultSet=Manager.cnx.request("
+// 		SELECT distinct TX.* from taxo AS TX
+// 		JOIN tagSpod AS TS ON TS.tag_id=TX.taxo_id
+// 		JOIN "+spodTable+" AS SP ON SP.id=TS.spod_id
+// 		WHERE TX.spodtype='"+spod.toLowerCase()+"'"
+// 		);
+		 
+// 		trace("resultSet="+resultSet +"spodTAble="+spodTable);
 		
-		trace("resultSet="+resultSet +"spodTAble="+spodTable);
 		
 		
-		
-	return cast resultSet.results();
-	//return search({spodtype:spod});
-}
- function getTag( tag:String , ?spod:String ) : Taxo
-{
-	return search({ tag : tag, spodtype:spod}).first();
-}
+// 	return cast resultSet.results();
+// 	//return search({spodtype:spod});
+// }
+//  function getTag( tag:String , ?spod:String ) : Taxo
+// {
+// 	return search({ tag : tag, spodtype:spod}).first();
+// }
 
-public function getSpodsByTag(tag:String,?spodstring:String) : List < Spodable >
-{
-	currentspod=firstUpperCase(spodstring);
-	//trace("currentSpod="+currentspod);
+// public function getSpodsByTag(tag:String,?spodstring:String) : List < Spodable >
+// {
+// 	currentspod=firstUpperCase(spodstring);
+// 	//trace("currentSpod="+currentspod);
 	
-	var tag_id=getTag(tag,spodstring.toLowerCase()).taxo_id;
-	//trace("tag_id="+tag_id);
+// 	var tag_id=getTag(tag,spodstring.toLowerCase()).taxo_id;
+// 	//trace("tag_id="+tag_id);
 	
-	var spodTable=getSpodTable(spodstring);
+// 	var spodTable=getSpodTable(spodstring);
 
-var resultSet=Manager.cnx.request("
-	SELECT  DISTINCT B.* from  "+spodTable+" AS B
-	LEFT JOIN `tagSpod` AS TS ON TS.`spod_id`=B.id 
-	LEFT JOIN  `taxo` AS TX ON TX.`taxo_id`= TS.`tag_id`  
-	WHERE TX.taxo_id="+tag_id
-	);
+// var resultSet=Manager.cnx.request("
+// 	SELECT  DISTINCT B.* from  "+spodTable+" AS B
+// 	LEFT JOIN `tagSpod` AS TS ON TS.`spod_id`=B.id 
+// 	LEFT JOIN  `taxo` AS TX ON TX.`taxo_id`= TS.`tag_id`  
+// 	WHERE TX.taxo_id="+tag_id
+// 	);
 	
 
-	var maped:List<Spodable>= resultSet.results().map(maptoSpod);
+// 	var maped:List<Spodable>= resultSet.results().map(maptoSpod);
 
-	//Lib.print(liste);
-	//Object
-	//return cast(resultSet.results());
-	return maped;
-//	Lib.print("resultSet="+resultSet.results().toString());
-//	return new List<Spodable>();
-//	return cast liste;
-}
+// 	//Lib.print(liste);
+// 	//Object
+// 	//return cast(resultSet.results());
+// 	return maped;
+// //	Lib.print("resultSet="+resultSet.results().toString());
+// //	return new List<Spodable>();
+// //	return cast liste;
+// }
 
- function maptoSpod(res:Dynamic) :Spodable{
-	var spod:Spodable= Type.createInstance(Type.resolveClass("vo."+currentspod),[]);
-	var formule= spod.getFormule();
-	Reflect.setField(spod, "id",Reflect.field(res,"id"));
-	for (key in formule.keys()){
-		trace("key="+key);
+//  function maptoSpod(res:Dynamic) :Spodable{
+// 	var spod:Spodable= Type.createInstance(Type.resolveClass("vo."+currentspod),[]);
+// 	var formule= spod.getFormule();
+// 	Reflect.setField(spod, "id",Reflect.field(res,"id"));
+// 	for (key in formule.keys()){
+// 		trace("key="+key);
 		
-	//	if( Reflect.field(res,key)!=null)
-		Reflect.setField(spod, key,Reflect.field(res,key));
-	}
-	return spod;
-	}
+// 	//	if( Reflect.field(res,key)!=null)
+// 		Reflect.setField(spod, key,Reflect.field(res,key));
+// 	}
+// 	return spod;
+// 	}
 
-public function getTagsBySpodID(spod:String,spod_id:Int) : List < Taxo >
-{
-	var spodTable=getSpodTable(spod);
-	var resultSet=Manager.cnx.request("
-	SELECT DISTINCT TX.taxo_id , TX.tag from `taxo` AS TX
-	LEFT JOIN `tagSpod` AS TS ON TS.`tag_id`=TX.`taxo_id`
-	LEFT JOIN "+spodTable+" AS B ON TS.`spod_id`= B.`id`
-	WHERE B.id="+spod_id+" AND TX.spodtype='"+spod+"'"
-	);
-	return cast (resultSet.results());
-}
+// public function getTagsBySpodID(spod:String,spod_id:Int) : List < Taxo >
+// {
+// 	var spodTable=getSpodTable(spod);
+// 	var resultSet=Manager.cnx.request("
+// 	SELECT DISTINCT TX.taxo_id , TX.tag from `taxo` AS TX
+// 	LEFT JOIN `tagSpod` AS TS ON TS.`tag_id`=TX.`taxo_id`
+// 	LEFT JOIN "+spodTable+" AS B ON TS.`spod_id`= B.`id`
+// 	WHERE B.id="+spod_id+" AND TX.spodtype='"+spod+"'"
+// 	);
+// 	return cast (resultSet.results());
+// }
 
 
-public function associate(tag:String,spod:String,spodId:Int){
-	var id=getTag(tag,spod).taxo_id;
-	Manager.cnx.request("insert into tagSpod (tag_id,spod_id) VALUES ("+id+","+spodId+") ");
-	//object("insert into tagSpod (tag_id,spod_id) VALUES ("+id+","+spodId+") ",true);
-}
-public function dissociate(tag:String,spod:String,spodId:Int) : Void {
-	var id=getTag(tag,spod).taxo_id;
-	Manager.cnx.request("DELETE  FROM tagSpod  WHERE tag_id="+id+" and spod_id="+spodId );
-}
-private function getSpodTable(spod:String):String{
-	trace("getSpoTable"+spod);
-	var voPackage="vo.";
-	var cap= firstUpperCase(spod);
+// public function associate(tag:String,spod:String,spodId:Int){
+// 	var id=getTag(tag,spod).taxo_id;
+// 	Manager.cnx.request("insert into tagSpod (tag_id,spod_id) VALUES ("+id+","+spodId+") ");
+// 	//object("insert into tagSpod (tag_id,spod_id) VALUES ("+id+","+spodId+") ",true);
+// }
 
-	var spodable:Spodable=cast Type.resolveClass(voPackage+cap);
-	trace("spodable"+spodable);
-	var manager:Manager<sys.db.Object>= cast Reflect.field(spodable,"manager");
-	trace("manager"+manager);
-	var spodinfos:SpodInfos= manager.dbInfos();
+// public function dissociate(tag:String,spod:String,spodId:Int) : Void {
+// 	var id=getTag(tag,spod).taxo_id;
+// 	Manager.cnx.request("DELETE  FROM tagSpod  WHERE tag_id="+id+" and spod_id="+spodId );
+// }
 
-	return spodinfos.name; /// added spod_macro specific getTble name via dbInfos
+// private function getSpodTable(spod:String):String{
+// 	trace("getSpoTable"+spod);
+// 	var voPackage="vo.";
+// 	var cap= firstUpperCase(spod);
+
+// 	var spodable:Spodable=cast Type.resolveClass(voPackage+cap);
+// 	trace("spodable"+spodable);
+// 	var manager:Manager<sys.db.Object>= cast Reflect.field(spodable,"manager");
+// 	trace("manager"+manager);
+// 	var spodinfos:SpodInfos= manager.dbInfos();
+
+// 	return spodinfos.name; /// added spod_macro specific getTble name via dbInfos
 
 	
-}
-function firstUpperCase(str:String) : String {
-var firstChar:String = str.substr(0, 1);
-var restOfString:String = str.substr(1, str.length);
-return firstChar.toUpperCase()+restOfString.toLowerCase();
-}
+// }
+// function firstUpperCase(str:String) : String {
+// var firstChar:String = str.substr(0, 1);
+// var restOfString:String = str.substr(1, str.length);
+// return firstChar.toUpperCase()+restOfString.toLowerCase();
+// }
 
 
 
