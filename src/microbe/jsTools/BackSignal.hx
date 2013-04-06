4554c1ccfd09044a9e6e8162b2f706674bea2b51
postite <blog@postite.com>
2013-04-06 15:46:30 +0200

    Moved string to INt

--- src/microbe/jsTools/BackSignal.hx
+++ src/microbe/jsTools/BackSignal.hx
@@ -2,13 +2,13 @@ package microbe.jsTools;
 import msignal.Signal;
 class BackSignal extends Signal1<String>
 {
-	public static  var preredirect:Signal1<String>;
-	public static  var complete:Signal1<String>;
+	public static  var preredirect:Signal1<Int>;
+	public static  var preredirectomplete:Signal1<String>;
 	
 	public function new()
 	{
 		preredirect= new Signal1();
-		complete= new Signal1();
+		preredirectomplete= new Signal1();
 		super(String);
 	}
 }
